7542a13d2fb71bebf1d73eca80bdcc7d4889a865
RubCast28 <rucasta08@gmail.com>
2025-03-18 16:32:19 +0100

    si

--- Proyecto_Equipo/src/controlador/Principal.java
+++ Proyecto_Equipo/src/controlador/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Pero que esta pasando Chavalada");
 		System.out.println(" sexo");
+		System.out.println("Si");
 	}
 
 }
