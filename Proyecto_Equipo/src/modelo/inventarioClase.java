f78ee8ddd806e4b1208744aabc63db59f15adbdb
Fabricio Lozada <espalvaro64@gmail.com>
2025-03-25 17:24:16 +0100

    Estoy llenando las clases

--- Proyecto_Equipo/src/modelo/inventarioClase.java
+++ Proyecto_Equipo/src/modelo/inventarioClase.java
@@ -1,5 +1,11 @@
 package modelo;
 
-public class inventarioClase {
+import java.util.ArrayList;
 
+public class inventarioClase {
+	private ArrayList<Producto> productos;
+	
+	public void Inventario() {
+		productos = new ArrayList<>();
+	}
 }
--- Proyecto_Equipo/src/modelo/usuario.java
+++ Proyecto_Equipo/src/modelo/usuario.java
@@ -1,5 +1,40 @@
 package modelo;
 
 public class usuario {
+	private String Usuario;
+	private String roll;
+	private String password;
+	
+	public usuario(String usuario, String roll, String password) {
+		Usuario = usuario;
+		this.roll = roll;
+		this.password = password;
+	}
 
+	public String getUsuario() {
+		return Usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		Usuario = usuario;
+	}
+
+	public String getRoll() {
+		return roll;
+	}
+
+	public void setRoll(String roll) {
+		this.roll = roll;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
 }
